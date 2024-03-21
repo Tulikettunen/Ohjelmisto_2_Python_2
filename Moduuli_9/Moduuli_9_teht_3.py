03085810fe8e6f8a1e5a73f1346f645caba1291a
Tulikettunen <tuuli.mrl@gmail.com>
2024-03-21 23:35:33 +0200

    Moduulin 9 tehtävät tehty, osa hieman yli tehtävänannon.

--- Moduuli_9/Moduuli_9_teht_3.py
+++ Moduuli_9/Moduuli_9_teht_3.py
@@ -8,6 +8,8 @@ Nopeus on 60 km/h. Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaa
 # ohjelmaa muokattu edellisestä sen verran, että myös nopeuden lisäykset voidaan laittaa
 # inputtina käyttäjältä
 
+# Hups, se vähän eskaloitu.
+
 
 class Auto:
     def __init__(self, rek_tunnus, top_speed):
@@ -16,34 +18,66 @@ class Auto:
         self.speed_now = 0
         self.distance = 0
 
+
     def info_print(self):
         print(f"Auton rekisteritunnus: {self.rek_tunnus}, \nhuippunopeus: {self.top_speed}, "
-              f"\nnopeus juuri nyt: {self.speed_now} \nja kuljettu matka: {self.distance}")
+              f"\nnopeus juuri nyt: {self.speed_now} \nja kuljettu matka: {self.distance}\n")
+
 
     def kiihdytä(self):
-        speed_change = int(input("Kuinka paljon nopeutta on lisätty? (km/h): \n >"))
-        if speed_change + self.speed_now > self.top_speed:
-            self.speed_now = self.top_speed
-            print(f"Olet saavuttanut maksiminopeuden, {self.top_speed} km/h.")
-        elif self.speed_now + speed_change < 0:
-            self.speed_now = 0
-            print("Olet jarruttanut auton pysähdyksiin, nopeutesi on 0km/h.")
-        elif speed_change > 80:
-            print(f"Auto {auto_1.rek_tunnus} teki hätä jarrutuksen, ja sen nykyinen nopeus on {auto_1.speed_now} km/h.")
-        else:
-            self.speed_now += speed_change
-            print(f"Uusi nopeutesi on {self.speed_now} km/h.")
+        y = True
+        while y:
+            try:
+                speed_change = int(input("Kuinka paljon nopeus on muuttunut? (km/h): \n >"))
+                if speed_change + self.speed_now > self.top_speed:
+                    self.speed_now = self.top_speed
+                    print(f"Olet saavuttanut maksiminopeuden, {self.top_speed} km/h.\n")
+                elif self.speed_now + speed_change < 0:
+                    self.speed_now = 0
+                    print("Olet jarruttanut auton pysähdyksiin, nopeutesi on 0km/h.\n")
+                else:
+                    self.speed_now += speed_change
+                    if speed_change < -80:
+                        print(f"Auto {auto_1.rek_tunnus} teki hätä jarrutuksen, ja sen nykyinen nopeus on {auto_1.speed_now} km/h.\n")
+                    else:
+                        print(f"Uusi nopeutesi on {self.speed_now} km/h.\n")
+                y = False
+            except ValueError:
+                print("Syötätkö tähän kenttään numeroita vaan, tänks.\n")
+
 
     def kulje(self):
         # tähän kohtaan pitäisi lisätä ehto että käyttäjä ei voi syöttää negatiivista aikaa
-        travel_time = float(input("Kuinka monta tuntia nykyisellä nopeudella on kuljettu?: \n >"))
-        self.distance += self.speed_now * travel_time
-        print(f"Matka {self.speed_now * travel_time} km on lisätty matkamittariisi. \n"
-              f"Nykyinen kuljettu kokonaismatkasi on {self.distance} km.")
+        e = True
+        while e:
+            try:
+                travel_time = float(input("Kuinka monta tuntia nykyisellä nopeudella on kuljettu?: \n >"))
+                if travel_time < 0:
+                    print("Idiootti, et voi matkustaa ajassa taaksepäin, sorry.\n")
+                else:
+                    self.distance += self.speed_now * travel_time
+                    print(f"Matka {self.speed_now * travel_time} km on lisätty matkamittariisi. \n"
+                          f"Nykyinen kuljettu kokonaismatkasi on {self.distance} km.\n")
+                    e = False
+            except ValueError:
+                print("Syötätkö tähän kohtaan numeroita vaan tänks.\n")
 
 auto_1 = Auto("ABC-123", 142)
 
+i = True
+while i:
+    choice = input("Haluatko muuttaa nopeutta (1) \n"
+                   "kertoa kauanko olet kulkenut kyseisellä nopeudella (2) \n"
+                   "tai Lopettaa? (any other key) \n>")
+    if choice == "1":
+        auto_1.kiihdytä()
+    elif choice == "2":
+        auto_1.kulje()
+    else:
+        i = False
 
+
+"""
 auto_1.kiihdytä()   # tässä kohtaa aluksi lisättiin nopeutta 30 km/h
 auto_1.kulje()      # laita tähän kohtaan haluamasi aika
 
@@ -55,7 +89,8 @@ auto_1.kulje()
 
 auto_1.kiihdytä()   # tässä kohtaa aluksi vähennettiin nopeutta -200 km/h
 auto_1.kulje()
+"""
 
-print(f"Auton tiedot matkan lopuksi: \n {auto_1.info_print()} .")
-
+print("Auton tiedot matkan lopuksi:")
+auto_1.info_print()
 
