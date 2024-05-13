380ee36b63949171c66f97a3f4abde11e9a0cf8b
tuuli <tuuli.mrl@gmail.com>
2024-05-13 04:01:36 +0300

    Moduuli 11 teht 2

--- Moduuli_11/Moduuli_11_teht_2.py
+++ Moduuli_11/Moduuli_11_teht_2.py
@@ -8,4 +8,41 @@ polttomoottoriauton (ACD-123, 165 km/h, 32.3 l). Aseta kummallekin autolle halua
 kolmen tunnin verran ja tulosta autojen matkamittarilukemat.
 """
 
-import Auto from
+class Car:
+    def __init__(self, rek_tunnus, top_speed):
+        self.rek_tunnus = rek_tunnus
+        self.top_speed = top_speed
+        self.speed_now = 0
+        self.distance = 0
+
+    def drive(self):
+        self.speed_now = random.randint(0, self.top_speed)
+        travel_time = int(input("How long do you travel? \n>"))
+        auto.distance += self.speed_now * travel_time
+
+    def info_print(self):
+        print(f"________________________________________\n"
+              f"Register plade: {self.rek_tunnus},\n roadometer count: {self.distance} km. ")
+
+
+
+class Electric(Car):
+    def __init__(self, rek_tunnus, top_speed, battery_capacity):
+        super().__init__(rek_tunnus, top_speed)
+        self.battery_capacity = battery_capacity
+
+    def info_print(self):
+        super().info_print()
+        print(f"Other information: \nBattery capacity: {self.battery_capacity}, ")
+
+
+
+class Fuel(Car):
+    def __init__(self, rek_tunnus, top_speed, gas_tank):
+        super().__init__(rek_tunnus, top_speed)
+        self.gas_tank = gas_tank
+
+    def info_print(self):
+        super().info_print()
+        print(f"Other information:
+
